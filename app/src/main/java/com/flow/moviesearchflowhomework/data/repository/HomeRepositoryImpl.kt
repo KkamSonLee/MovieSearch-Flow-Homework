76519100ab646d4c79750d658d6076d6eea92c2b
minho <my_alsgh@naver.com>
2023-01-20 03:05:39 +0900

    [CHORE] Preparing Setup to Paging remove datasource

--- app/src/main/java/com/flow/moviesearchflowhomework/data/repository/HomeRepositoryImpl.kt
+++ app/src/main/java/com/flow/moviesearchflowhomework/data/repository/HomeRepositoryImpl.kt
@@ -1,16 +1,20 @@
 package com.flow.moviesearchflowhomework.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.flow.moviesearchflowhomework.data.datasource.LocalRecentSearchDataSource
-import com.flow.moviesearchflowhomework.data.datasource.RemoteSearchDataSource
 import com.flow.moviesearchflowhomework.data.mapper.SearchItemMapper
+import com.flow.moviesearchflowhomework.data.service.NaverService
 import com.flow.moviesearchflowhomework.domain.entity.RecentSearchKeywordEntity
 import com.flow.moviesearchflowhomework.domain.entity.SearchItem
+import com.flow.moviesearchflowhomework.data.datasource.SearchPagingSource
 import com.flow.moviesearchflowhomework.domain.repository.HomeRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class HomeRepositoryImpl @Inject constructor(
-    private val remoteSearchDatasource: RemoteSearchDataSource,
+    private val naverService: NaverService,
     private val localRecentSearchDataSource: LocalRecentSearchDataSource,
     private val searchItemMapper: SearchItemMapper
 ) : HomeRepository {
--- app/src/main/java/com/flow/moviesearchflowhomework/di/DataSourceModule.kt
+++ app/src/main/java/com/flow/moviesearchflowhomework/di/DataSourceModule.kt
@@ -2,8 +2,6 @@ package com.flow.moviesearchflowhomework.di
 
 import com.flow.moviesearchflowhomework.data.datasource.LocalRecentSearchDataSource
 import com.flow.moviesearchflowhomework.data.datasource.LocalRecentSearchDataSourceImpl
-import com.flow.moviesearchflowhomework.data.datasource.RemoteSearchDataSource
-import com.flow.moviesearchflowhomework.data.datasource.RemoteSearchDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -13,10 +11,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DataSourceModule {
-    @Binds
-    @Singleton
-    abstract fun bindRemoteSearchDataSource(remoteSearchDataSource: RemoteSearchDataSourceImpl): RemoteSearchDataSource
-
     @Binds
     @Singleton
     abstract fun bindLocalSearchDataSource(localRecentSearchDataSource: LocalRecentSearchDataSourceImpl): LocalRecentSearchDataSource
