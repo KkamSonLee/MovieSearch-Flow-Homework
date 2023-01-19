4d78e96ff3cafcf4ad011fd72972bf7083070b7d
minho <my_alsgh@naver.com>
2023-01-20 02:32:54 +0900

    [CHORE] Safe api Call Add

---
+++ app/src/main/java/com/flow/moviesearchflowhomework/data/util/Extension.kt
@@ -0,0 +1,26 @@
+package com.flow.moviesearchflowhomework.data.util
+
+import android.util.Log
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+suspend inline fun <T> catchingApiCall(crossinline apiCall: suspend () -> Response<T>): T? {
+    return runCatching {
+        apiCall().body()
+    }.getOrElse { throwable ->
+        when (throwable) {
+            is IOException -> {         //SocketConnection, Timeout Error Catch, 보통 핸드쉐이크 관련
+                Log.e("IOException", throwable.message.toString())
+                null
+            }
+            is HttpException -> {       //error 코드까지는 내려옴, end point까지는 접근
+                Log.e("HttpException", throwable.message.toString())
+                null
+            }
+            else -> {
+                null
+            }
+        }
+    }
+}
